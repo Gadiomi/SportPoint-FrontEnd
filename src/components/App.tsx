15bbab072eefa3ecb2e2e2e9a8dccc8e23706d2b
d-sofiika <soniadrozd2709@gmail.com>
2025-02-25 00:18:26 +0200

    added redux and PrivateRoutes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,13 @@
 import { lazy, Suspense, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import PrivateRoute from "./PrivateRoutes";
 
 const Home = lazy(() => import("../pages/HomePage/HomePage"));
 const LogIn = lazy(() => import("../pages/LogInPage/LogInPage"));
 const Register = lazy(() => import("../pages/RegisterPage/RegisterPage"));
 const Details = lazy(() => import("../pages/DetailsPage/DetailsPage"));
 const Favorites = lazy(() => import("../pages/FavoritesPage/FavoritesPage"));
+const Account = lazy(() => import("../pages/AccountPage/AccountPage"))
 
 function App() {
   return (
@@ -17,6 +16,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/register" element={<Register />} />
+        <Route element={<PrivateRoute />}>
+           <Route path="/account" element={<Account />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Route>
       </Routes>
     </Suspense>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,8 @@ import css from "./HomePage.module.css"
 
 const HomePage = () => {
   return (
-    <div className={css.section}>
+      <div className={css.section}>
+          <h1>Home</h1>
     </div>
   );
 };
