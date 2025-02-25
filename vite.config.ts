46f42f252457328573549f8c9e80bbe4b82077ad
Maksym <fivemoshr@gmail.com>
2025-02-25 14:58:04 +0200

    feat: enhance TypeScript configuration with path aliases and type roots; update Vite config for build output and alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  resolve: {
+    alias: {
+      components: '/src/components',
+      theme: '/src/theme',
+      hooks: '/src/hooks',
+      pages: '/src/pages',
+    },
+  },
+  build: {
+    outDir: './dist',
+    assetsDir: './dist/assets',
+  },
 });
