fa47ed204fe19fc414af7cb7a1c7d6a52499d4be
Maksym <fivemoshr@gmail.com>
2025-03-02 12:46:17 +0200

    feat: add Icon component with documentation and constants

---
+++ src/kit/Icon/Icon.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { IconName } from './constants';
+
+export interface IconProps {
+  name: IconName;
+  color?: string;
+  width?: string;
+  height?: string;
+  size?: string | number;
+}
+
+export const Icon = React.memo(
+  ({
+    name,
+    color = 'black',
+    height = '30px',
+    width = '30px',
+    size,
+  }: IconProps) => {
+    const iconLink = `/assets/svg/symbol-defs.svg#${name}`;
+
+    return (
+      <svg height={size || height} width={size || width} color={color}>
+        {React.createElement('use', {
+          href: iconLink,
+          xlinkHref: iconLink,
+        })}
+      </svg>
+    );
+  },
+);
---
+++ src/kit/Icon/constants.ts
@@ -0,0 +1,4 @@
+export enum IconName {
+  CHECK = 'check',
+  CROSS = 'cross',
+}
