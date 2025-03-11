4f322f89739f76d374f648cb7f858919a8aa5bc2
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-03-11 17:27:03 +0200

    kit-typography

--- src/kit/index.ts
+++ src/kit/index.ts
@@ -1,3 +1,22 @@
 export { Icon, IconName } from './Icon';
 export { Button } from './Button/Button';
-export { Modal } from './modal';
+export { Modal } from './Modal/Modal';
+export {
+  Title,
+  Subtitle,
+  Name,
+  Main,
+  Medium,
+  Light,
+  ButtonTypogr,
+  Card,
+  Small,
+  About,
+  PopUp,
+} from './Typography/Typography';
+export {
+  FontSizes,
+  FontWeights,
+  LineHeights,
+  FontFamily,
+} from './Typography/constants';
