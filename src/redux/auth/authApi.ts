4894df3fb2e07d9b44d9b2479613080310bea205
Maksym <fivemoshr@gmail.com>
2025-03-20 14:09:44 +0200

    refine: update authentication API endpoints and enhance token management with axios interceptors

--- src/redux/auth/authApi.ts
+++ src/redux/auth/authApi.ts
@@ -3,31 +3,33 @@ import { axiosBaseQuery } from './axios';
 
 export const authApi = createApi({
   reducerPath: 'Auth',
-  baseQuery: axiosBaseQuery({
-    baseURL: 'https://readjourney.b.goit.study/api/',
-  }),
+  baseQuery: axiosBaseQuery(),
   endpoints: (builder: any) => ({
     register: builder.mutation({
       query: (userData: { name: string; email: string; password: string }) => ({
-        url: 'users/signup',
+        url: 'auth/signup',
         method: 'POST',
         data: userData,
       }),
+
       providesTags: ({ email }: { email: string }) => [
         { type: 'Auth', id: email },
       ],
     }),
     login: builder.mutation({
-      query: (userData: { email: string; password: string }) => ({
-        url: 'users/signin',
-        method: 'POST',
-        data: userData,
-      }),
+      query: (userData: { email: string; password: string }) => {
+        return {
+          url: 'auth/signin',
+          method: 'POST',
+          data: userData,
+        };
+      },
       providesTags: ({ email }: { email: string }) => [
         { type: 'Auth', id: email },
       ],
     }),
   }),
+
   tagTypes: ['Auth'],
 });
 
