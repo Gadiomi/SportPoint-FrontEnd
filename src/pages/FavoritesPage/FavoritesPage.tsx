8c256e1d1f48556bdc3aa5fa2a7fd93c63fa5a66
Serzh108 <136076826+Serzh108Tetra@users.noreply.github.com>
2025-03-27 16:38:14 +0200

    before rebase

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -15,9 +15,6 @@ import { FavoritesPageWrapper, ListWrapper, ToggleWrapper } from './styles';
 import CoachCard from '../../components/CoachCard/CoachCard';
 import ClubCard from './ClubCard/ClubCard';
 import { IClubData, ICoachData } from '../../types';
-// --- TEMP 2 ---
-import axios from 'axios';
-// --- TEMP 2 ---
 
 // --- TEMP!!! ---
 const coachsData: ICoachData[] = [
@@ -64,127 +61,6 @@ const clubsData: IClubData[] = [
 const itemsPerPage = 2;
 
 const FavoritesPage = () => {
-  // --- * TEMP!!! for testing! ** ---
-  interface TsignInData {
-    email: string;
-    password: string;
-  }
-  interface TsignUpData extends TsignInData {
-    role: string;
-  }
-
-  const signUpData = {
-    // email: 'serzh108@ukr.net',
-    email: 'ser2015@i.ua',
-    password: 'MyPass1004',
-    role: 'customer',
-  };
-
-  const signInData = {
-    // email: 'serzh108@ukr.net',
-    email: 'ser2015@i.ua',
-    password: 'MyPass1004',
-  };
-
-  const BASE_URL = 'https://sportpoint-backend.onrender.com/';
-
-  const axiosPublic = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    // withCredentials: false,
-    withCredentials: true,
-  });
-
-  const ENDPOINTS = {
-    SIGN_UP: 'auth/signup',
-    LOGIN: 'auth/signin',
-    USER_PROFILE: '/profile',
-  };
-
-  const singUp = async (singUp: TsignUpData) => {
-    console.log(' --- in singUp ');
-    try {
-      const result = await axiosPublic.post(ENDPOINTS.SIGN_UP, singUp);
-      return result.status;
-    } catch (error) {
-      return { message: error };
-    }
-  };
-
-  const singIn = async (logInData: TsignInData) => {
-    console.log(' --- in singIn ');
-    try {
-      const result = await axiosPublic.post(ENDPOINTS.LOGIN, logInData);
-      return result;
-      //  return result.status;
-    } catch (error) {
-      return { message: error };
-    }
-  };
-
-  const userProfile = async () => {
-    console.log(' --- in userProfile ');
-    try {
-      const result = await axiosPublic.get(ENDPOINTS.USER_PROFILE);
-      return result;
-      //  return result.status;
-    } catch (error) {
-      return { message: error };
-    }
-  };
-
-  const singUpRequest = async (data: TsignUpData) => {
-    console.log('data in singUpRequest -> ', data);
-    const result = await singUp(data);
-    console.log('result -> ', result);
-    if (result === 200) {
-      //   setIsAuth(true);
-      // reset();
-      // setLoginError(false);
-      // router.push('/home');
-    } else {
-      // setLoginError(true);
-      console.log('SignUp Error!');
-    }
-  };
-  // --- Temp!!! ---
-  const singInRequest = async (data: TsignInData) => {
-    console.log('data in singInRequest -> ', data);
-    const result = await singIn(data);
-    console.log('result -> ', result);
-    if (result) {
-      // if (result === 200) {
-      //   setIsAuth(true);
-      // reset();
-      // setLoginError(false);
-      // router.push('/home');
-    } else {
-      // setLoginError(true);
-      console.log('SignIn Error!');
-    }
-  };
-
-  const getUserProfile = async () => {
-    console.log('data in singInRequest -> ');
-    const result = await userProfile();
-    console.log(' -- user result -> ', result);
-    if (result) {
-    } else {
-      console.log(' getUserProfile Error!');
-    }
-  };
-
-  const tempClickHandler = () => {
-    singInRequest(signInData);
-  };
-
-  const userProfileHandler = () => {
-    getUserProfile();
-  };
-  // --- /Temp!!! ---
-  // --- / *** ---
   const [isActive, setIsActive] = useState<boolean>(true);
   const [coachPageNumber, setCoachPageNumber] = useState<number>(0);
   const [clubPageNumber, setClubPageNumber] = useState<number>(0);
@@ -195,16 +71,7 @@ const FavoritesPage = () => {
     isActive
       ? setCoachPageNumber(prev => prev + 1)
       : setClubPageNumber(prev => prev + 1);
-    // ---
-    // singUpRequest(signUpData); // Temp for test!!!
   };
-
-  // console.log(
-  //   ' - coachPageNumber: ',
-  //   coachPageNumber,
-  //   ' -- clubPageNumber: ',
-  //   clubPageNumber,
-  // );
 
   return (
     <Section>
@@ -304,13 +171,6 @@ const FavoritesPage = () => {
               <div>Ще нічого не обрано!</div>
             )}
             <p onClick={showMore}>{t('show_more')}</p>
-            {/* --- */}
-            <p onClick={tempClickHandler} style={{ marginTop: '16px' }}>
-              LOGIN
-            </p>
-            <p onClick={userProfileHandler} style={{ marginTop: '16px' }}>
-              User profile
-            </p>
           </ListWrapper>
         </FavoritesPageWrapper>
       </Container>
