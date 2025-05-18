6e75f6685852bcc5ba3e72116f3cbaf92e87679b
d-sofiika <soniadrozd2709@gmail.com>
2025-05-18 22:16:34 +0300

    fix: A little fix with translate and error message

--- src/pages/HomePage/components/ClubsList/ClubsList.tsx
+++ src/pages/HomePage/components/ClubsList/ClubsList.tsx
@@ -39,11 +39,11 @@ export const ClubsList: React.FC = () => {
   console.log('first', data);
   return (
     <Container styles={{ alignItems: 'flex-end', padding: '16px 0px' }}>
-      {error ? <Loading>{t('home_page.error')}</Loading> : null}
       {isLoading ? (
         <Loading>{t('home_page.loading')}...</Loading>
       ) : (
         <>
+          {error ? <Loading>{t('home_page.error')}</Loading> : null}
           <StyledButtonBack
             onClick={() => navigate(`/`)}
             testId="clubsBack"
